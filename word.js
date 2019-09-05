4a9caaa0d19653432f1666eccf7dd9daba2cb615
Bilal Sarımeşeli <bilalsarimeseli@Bilals-Air.attlocal.net>
2019-09-04 22:33:24 -0700

    the logic has bbeen completed

--- word.js
+++ word.js
@@ -1,27 +1,27 @@
 var Letter = require("./letter.js");
 
 function Word(answer) {
+    //Letter objects array
     this.objArray = [];
 
-    for(var i = 0; i < objArray.length; i++) {
+    for (var i = 0; i < answer.length; i++) {
         var letter = new Letter(answer[i]);
         this.objArray.push(letter);
     }
 
+    this.log = function () {
+        answerLog = "";
+        for (var i = 0; i < this.objArray.length; i++) {
+            answerLog += this.objArray[i] + " ";
+        }
+        console.log(answerLog + "\n========================\n");
+    };
 
-    this.log = function() {
-        var answerLog = "";
-            for(var i = 0; i < objArray.length; i++) {
-                answerLog += this.objArray[i] + " ";
-            }
-        console.log(answerLog + "\n========================\n");   
-    }
-
-    this.userGuess = function(input) {
-        for(var i = 0; i < this.objArray.length; i++) {
+    this.userGuess = function (input) {
+        for (var i = 0; i < this.objArray.length; i++) {
             this.objArray[i].guess(input);
         }
-    }
+    };
 }
 
 module.exports = Word;
